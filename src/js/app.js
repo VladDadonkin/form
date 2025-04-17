cbe0ee508412a6892fc59d12beb22510a633b3b0
VladDadonkin <vlad.dadonkin.02@mail.ru>
2025-04-18 00:43:19 +0300

    upd 0.0.1

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,23 @@
 
-const form = document.querySelector('.form');
+const btn = document.querySelector('.btn');
 const popover = document.querySelector('.popover');
 
-form.addEventListener('click', (e) => {
+btn.addEventListener('click', (e) => {
     e.preventDefault();
     if (popover.classList.contains('hidden')) {
+        const { right, top } = btn.getBoundingClientRect();
+        btn.style.left = right + 5 + 'px';
+        btn.style.top = top + btn.offsetHeight / 2 - btn.offsetHeight / 2 + 'px';
+
+
         popover.classList.remove('hidden')
+        console.log(right, top);
     }
     else {
         popover.classList.add('hidden')
     }
 
-
+    
     console.log('submit');
 })
 
